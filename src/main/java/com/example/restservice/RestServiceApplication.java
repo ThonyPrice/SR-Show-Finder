d45979fe3ac4cf0c5eea89d6b16c82e5a9014a73
Thony Price <thonyprice@gmail.com>
2021-06-14 20:16:05 +0200

    Order imports and add comment

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,13 +1,16 @@
 package com.example.restservice;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
+/**
+ * A RESTful API that extends https://sverigesradio.se/api/documentation/v2/ 
+ * to enable searching for radio shows.
+ * */
 @SpringBootApplication
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
 	}
-
 }

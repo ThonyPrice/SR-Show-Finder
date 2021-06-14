c346940f7c7681357f14a0d32577e1aa86080695
Thony Price <thonyprice@gmail.com>
2021-06-15 01:24:41 +0200

    include Executor to manage maximum threads

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,16 +1,33 @@
 package com.example.restservice;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 /**
  * A RESTful API that extends https://sverigesradio.se/api/documentation/v2/ 
  * to enable searching for radio shows.
  * */
 @SpringBootApplication
+@EnableAsync
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+	@Bean
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(2);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("SRpublicApiCall-");
+		executor.initialize();
+		return executor;
 	}
 }

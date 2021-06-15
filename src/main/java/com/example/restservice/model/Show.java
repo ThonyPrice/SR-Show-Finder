bc1ba5bd404279e3571976af8b465b5328865663
Thony Price <thonyprice@gmail.com>
2021-06-15 09:59:14 +0200

    rm unused imports

--- src/main/java/com/example/restservice/model/Show.java
+++ src/main/java/com/example/restservice/model/Show.java
@@ -1,9 +1,7 @@
 package com.example.restservice.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.Arrays;
 import java.util.ArrayList;
 
 /**

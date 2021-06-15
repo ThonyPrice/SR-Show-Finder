7d7569abecfd02d52b6f689c4be13ea7ebd40b19
Thony Price <thonyprice@gmail.com>
2021-06-15 12:36:54 +0200

    rm unuset get/set

--- src/main/java/com/example/restservice/model/Show.java
+++ src/main/java/com/example/restservice/model/Show.java
@@ -15,23 +15,6 @@ import java.util.ArrayList;
 public class Show {
 	private String description;
 	private String broadcastinfo;
-	private String email;
-	private String phone;
-	private String programurl;
-	private String programslug;
-	private String programimage;
-	private String programimagetemplate;
-	private String programimagewide;
-	private String programimagetemplatewide;
-	private String socialimage;
-	private String socialimagetemplate;
-	ArrayList<Object> socialmediaplatforms = new ArrayList<Object>();
-	Channel ChannelObject;
-	private boolean archived;
-	private boolean hasondemand;
-	private boolean haspod;
-	private String responsibleeditor;
-	private float id;
 	private String name;
 
 
@@ -43,70 +26,6 @@ public class Show {
 
 	public String getBroadcastInfo() {
 		return broadcastinfo;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public String getProgramurl() {
-		return programurl;
-	}
-
-	public String getProgramslug() {
-		return programslug;
-	}
-
-	public String getProgramimage() {
-		return programimage;
-	}
-
-	public String getProgramimagetemplate() {
-		return programimagetemplate;
-	}
-
-	public String getProgramimagewide() {
-		return programimagewide;
-	}
-
-	public String getProgramimagetemplatewide() {
-		return programimagetemplatewide;
-	}
-
-	public String getSocialimage() {
-		return socialimage;
-	}
-
-	public String getSocialimagetemplate() {
-		return socialimagetemplate;
-	}
-
-	public Channel getChannel() {
-		return ChannelObject;
-	}
-
-	public boolean getArchived() {
-		return archived;
-	}
-
-	public boolean getHasondemand() {
-		return hasondemand;
-	}
-
-	public boolean getHaspod() {
-		return haspod;
-	}
-
-	public String getResponsibleeditor() {
-		return responsibleeditor;
-	}
-
-	public float getId() {
-		return id;
 	}
 
 	public String getName() {
@@ -121,70 +40,6 @@ public class Show {
 
 	public void setBroadcastInfo( String broadcastinfo ) {
 		this.broadcastinfo = broadcastinfo;
-	}
-
-	public void setEmail( String email ) {
-		this.email = email;
-	}
-
-	public void setPhone( String phone ) {
-		this.phone = phone;
-	}
-
-	public void setProgramurl( String programurl ) {
-		this.programurl = programurl;
-	}
-
-	public void setProgramslug( String programslug ) {
-		this.programslug = programslug;
-	}
-
-	public void setProgramimage( String programimage ) {
-		this.programimage = programimage;
-	}
-
-	public void setProgramimagetemplate( String programimagetemplate ) {
-		this.programimagetemplate = programimagetemplate;
-	}
-
-	public void setProgramimagewide( String programimagewide ) {
-		this.programimagewide = programimagewide;
-	}
-
-	public void setProgramimagetemplatewide( String programimagetemplatewide ) {
-		this.programimagetemplatewide = programimagetemplatewide;
-	}
-
-	public void setSocialimage( String socialimage ) {
-		this.socialimage = socialimage;
-	}
-
-	public void setSocialimagetemplate( String socialimagetemplate ) {
-		this.socialimagetemplate = socialimagetemplate;
-	}
-
-	public void setChannel( Channel channelObject ) {
-		this.ChannelObject = channelObject;
-	}
-
-	public void setArchived( boolean archived ) {
-		this.archived = archived;
-	}
-
-	public void setHasondemand( boolean hasondemand ) {
-		this.hasondemand = hasondemand;
-	}
-
-	public void setHaspod( boolean haspod ) {
-		this.haspod = haspod;
-	}
-
-	public void setResponsibleeditor( String responsibleeditor ) {
-		this.responsibleeditor = responsibleeditor;
-	}
-
-	public void setId( float id ) {
-		this.id = id;
 	}
 
 	public void setName( String name ) {
